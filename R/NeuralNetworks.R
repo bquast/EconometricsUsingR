a37ad33be2bbdf694fd941900314c02ba72c2e6c
Bastiaan Quast <bquast@gmail.com>
2018-01-05 13:39:47 +0100

    write backpropagation() and export

--- R/NeuralNetworks.R
+++ R/NeuralNetworks.R
@@ -1,7 +1,35 @@
 #' Backpropagation
 #' @name backpropagation
+#' @export
 
 backpropagation <- function(X, Y) {
+  n          = length(Y)
+  input_dim  = dim(X)[2] # the number of columns or variables in X
+  output_dim = 1 # the number of outcome variables, here 1
+
+  # initialize weights randomly between -1 and 1, with mean 0
+  synapse_0  = matrix( runif(n = input_dim*output_dim, min=-1, max=1),
+                       nrow=input_dim,   # for dot multiplication, a %*% b, we need the number of rows of b to be equal to the number of columns of a (i.e. the number of input variables)
+                       ncol=output_dim ) # for dot multiplication, b %*% c, we need the number of columns of b to be equal to the number of rows of c (i.e. the number of output variables)
+
+  for (j in 1:n) {
+    # Feed forward through layers 0 and 1
+    layer_0 = X[j, , drop=FALSE]
+    layer_1 = sigmoid( layer_0 %*% synapse_0 )
+
+    # how much did we miss the target value?
+    layer_1_error = Y[j] - layer_1
+
+    if (j %% 10000 == 0)
+      print(paste("Error:", mean(abs(layer_1_error))))
+
+    # in what direction is the target layer_1?
+    # were we really sure? if so, don't change too much.
+    layer_1_delta = layer_1_error * sig_to_der(layer_1)
+
+    # update weights
+    synapse_0 = synapse_0 + t(layer_0) %*% layer_1_delta
+  }
 
 }
 
@@ -71,3 +99,13 @@ gradientdescent_data <- function(n=50000) {
 recurrentneuralnetwork_data <- function(n=50000) {
 
 }
+
+#' Sigmoid
+#' @name sigmoid
+sigmoid <- function(x)
+  1 / ( 1+exp(-x) )
+
+#' Sigmoid to Derivative
+#' @name sig_to_der
+sig_to_der <- function(x)
+  x*(1-x)
