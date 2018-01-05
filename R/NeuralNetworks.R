8f8de3a1e00265529c8572b315165fe872f5ba41
Bastiaan Quast <bquast@gmail.com>
2018-01-05 13:12:08 +0100

    setup functions and documentation for "Neural Networks"

---
+++ R/NeuralNetworks.R
@@ -0,0 +1,55 @@
+#' Backpropagation
+#' @name backpropagation
+
+backpropagation <- function(X, Y) {
+
+}
+
+#' Deep Learning
+#' @name deeplearning
+
+deeplearning <- function(X, Y) {
+
+}
+
+#' Gradient Descent
+#' @name gradientdescent
+
+gradientdescent <- function(X, Y) {
+
+}
+
+#' Recurrent Neural Network
+#' @name recurrentneuralnetwork
+
+recurrentneuralnetwork <- function(X, Y) {
+
+}
+
+#' Generate Backpropagation Data
+#' @name backpropagation_data
+
+backpropagation_data <- function(n=50000) {
+
+}
+
+#' Generate Deep Learning Data
+#' @name deeplearning_data
+
+deeplearning_data <- function(n=50000) {
+
+}
+
+#' Generate Gradient Descent Data
+#' @name gradientdescent_data
+
+gradientdescent_data <- function(n=50000) {
+
+}
+
+#' Generate Recurrent Neural Network Data
+#' @name recurrentneuralnetwork_data
+
+recurrentneuralnetwork_data <- function(n=50000) {
+
+}
