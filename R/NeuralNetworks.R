8d1dfea0f448c3a39ec7c89832b3836b800793d0
Bastiaan Quast <bquast@gmail.com>
2018-01-05 13:30:39 +0100

    add and export backpropagation_data()

--- R/NeuralNetworks.R
+++ R/NeuralNetworks.R
@@ -28,9 +28,27 @@ recurrentneuralnetwork <- function(X, Y) {
 
 #' Generate Backpropagation Data
 #' @name backpropagation_data
+#' @export
 
 backpropagation_data <- function(n=50000) {
+  # begin random numbers in the same spot
+  set.seed(123)
 
+  # generate
+  x1 <- rbinom(50000, 1, 0.5)
+  x2 <- rbinom(50000, 1, 0.5)
+  x3 <- rbinom(50000, 1, 0.5)
+
+  X <<- cbind(x1, x2, x3)
+
+  Y <<- x1
+
+  # print notice that objects X and Y are now available
+  print("The objects X and Y are now available in the Global Environment.")
+  print("Below the first few rows are printed together.")
+
+  # print the top of the data
+  return(head( cbind(Y,X) ))
 }
 
 #' Generate Deep Learning Data
